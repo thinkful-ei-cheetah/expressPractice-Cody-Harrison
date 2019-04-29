dae2c47c6ce141f10005c7b5ab02cc9701afdb27
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-04-29 13:41:12 -0700

    added lotto

--- app.js
+++ app.js
@@ -36,3 +36,43 @@ app.get('/cipher',(req,res)=>{
   let output = numbers.map(num=>String.fromCharCode(num)).join('');
   res.status(200).send(`Encoded text: ${output}`);
 })
+app.get('/lotto',(req,res)=>{
+  const {arr}=req.query;
+  const numbers = arr.map(a=>Number(a));
+  if(!arr){
+    res.status(200).send('must have input')
+  }
+  if(arr.length<6){
+      res.status(200).send('must have 6 numbers')
+    }
+  numbers.forEach(a=>{
+      if(typeof a!=='number'){
+        res.status(200).send('must be number')
+      }
+      if(a>20){
+        res.status(200).send('must not greater than 20')
+      }
+    })
+    const stockNumbers = Array(20).fill(1).map((_, i) => i + 1);
+
+    const winningNumbers = [];
+    for(let i = 0; i < 6; i++) {
+      const ran = Math.floor(Math.random() * stockNumbers.length);
+      winningNumbers.push(stockNumbers[ran]);
+      stockNumbers.splice(ran, 1);
+    }
+    const win=winningNumbers.filter(num=>numbers.includes(num));
+    console.log(winningNumbers,win)
+    // (`match ${win.length} numbers`);
+    switch(win.length){
+      case 6:
+      res.status(200).send('Wow! Unbelievable! You could have won the mega millions!');
+      case 5:
+      res.status(200).send('Congratulations! You win $100!');
+      case 4:
+      res.status(200).send('Free Ticket');
+      default:
+      res.status(200).send('Sorry you lose');
+
+    }
+})
